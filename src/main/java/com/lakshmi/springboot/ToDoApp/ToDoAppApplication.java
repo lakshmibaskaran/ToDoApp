dfc63ba39ae415ded178656e8d911c4b5da889e1
lakshmibaskaran <40994781+lakshmibaskaran@users.noreply.github.com>
2022-11-05 17:57:15 -0400

    1. Created an AppController for the ToDoApplication to run basic tests for RequestMapping and ResponseMessage

--- src/main/java/com/lakshmi/springboot/ToDoApp/ToDoAppApplication.java
+++ src/main/java/com/lakshmi/springboot/ToDoApp/ToDoAppApplication.java
@@ -2,13 +2,52 @@ package com.lakshmi.springboot.ToDoApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class ToDoAppApplication {
 
+public class ToDoAppApplication
+{
 	public static void main(String[] args)
 	{
 		SpringApplication.run(ToDoAppApplication.class, args);
 	}
+}
+
+
+
+/*
+@SpringBootApplication
+public class MyApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MyApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyApplication.class, args);
+	}
 
 }
+
+*/
+
+/*
+@SpringBootApplication
+//@SpringBootApplication(scanBasePackages = "com.lakshmi.springboot.ToDoApp")
+public class ToDoAppApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return application.sources(ToDoAppApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoAppApplication.class);
+	}
+}
+*/
+
