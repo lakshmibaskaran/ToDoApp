78c3df522147d3b72bf93d365cb68303661b56ce
lakshmibaskaran <40994781+lakshmibaskaran@users.noreply.github.com>
2022-12-01 16:30:44 -0500

    1. Login JSP file

---
+++ src/main/java/com/lakshmi/springboot/ToDoApp/Service/AuthenticationService.java
@@ -0,0 +1,31 @@
+package com.lakshmi.springboot.ToDoApp.Service;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService
+{
+    private Logger logger = LoggerFactory.getLogger((getClass()));
+
+
+    public boolean authenticateUser (String userName, String password)
+    {
+        logger.debug("Username is:" + userName);
+        logger.debug("Password is:" + password);
+
+        if (userName.equals("Nisha") && password.equals("ilikethis"))
+        {
+
+            logger.debug("Username matches with password");
+            return true;
+        }
+        else
+        {
+            logger.debug("Username and password do not match");
+            return false;
+        }
+    }
+}
