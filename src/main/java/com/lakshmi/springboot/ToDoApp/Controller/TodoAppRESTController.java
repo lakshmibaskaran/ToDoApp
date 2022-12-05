cf0a3204324891335e7c129520c3002d8c364b66
lakshmibaskaran <40994781+lakshmibaskaran@users.noreply.github.com>
2022-12-05 17:31:03 -0500

    Created a new REST Controller that handles all the APIs for Todo App

---
+++ src/main/java/com/lakshmi/springboot/ToDoApp/Controller/TodoAppRESTController.java
@@ -0,0 +1,35 @@
+package com.lakshmi.springboot.ToDoApp.Controller;
+
+
+
+import com.lakshmi.springboot.ToDoApp.Service.ToDoService;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+
+//The Serivce class is defined as a Rest Controller to get the list of Todos
+@RestController
+@RequiredArgsConstructor
+public class TodoAppRESTController
+{
+
+    private Logger logger = LoggerFactory.getLogger((getClass()));
+    //Annotation @RequiredArgsConstructor creates the boilerplate to define the constructor for TodoAppRESTController with ToDoService as the parameter
+    private final ToDoService toDoService;
+
+    @RequestMapping ("api/GetTodos")
+    public List getTodos()
+    {
+        //Return the list of Todos
+        logger.debug("Rest Controller to return the list of Todos");
+        // ToDoService toDoService = new ToDoService();
+
+        List listofToDos = toDoService.getToDoList();
+        return listofToDos;
+    }
+}
