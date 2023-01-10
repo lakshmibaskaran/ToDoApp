61205b066ace7d859c8eeb91fa93f1ce3a81d396
lakshmibaskaran <40994781+lakshmibaskaran@users.noreply.github.com>
2023-01-10 07:42:26 -0500

    1. Imported Springboot Security framework for the purpose of authentication and authorization. 2. Changed LoginController to WelcomeController (after introducing Springboot Security framework, the Login is abstracted away from the project and left for the Security framework to handle 3. Remove reference to Authentication Service as the Authentication Service to validate username and password will no longer be used. 4. RequestMapping is modified from /login.jsp to / (root URL). This is done to ensure we will no longer have login.jsp since Springboot Security framework takes care of Authentication. 5. deprecated an existing method goToWelcomePage because after authentication through Springboot security framework, the root URL calls the new "goToWelcomePage" 6. Added a new method getLoggedInUserName() to get the logged in username from the SecurityContextHolder.

--- src/main/java/com/lakshmi/springboot/ToDoApp/Controller/WelcomeController.java
+++ src/main/java/com/lakshmi/springboot/ToDoApp/Controller/WelcomeController.java
@@ -1,36 +1,48 @@
 package com.lakshmi.springboot.ToDoApp.Controller;
 
 
-import com.lakshmi.springboot.ToDoApp.Service.AuthenticationService;
+//import com.lakshmi.springboot.ToDoApp.Service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
 @RequiredArgsConstructor
 @SessionAttributes("name")
-public class LoginUIController
+public class WelcomeController
 {
     private Logger logger = LoggerFactory.getLogger((getClass()));
 
     //@RequiredArgsConstructor from lombok auto generates boiler plate code
     //for single argument constructor
-    private final AuthenticationService authenticationService;
+    //private final AuthenticationService authenticationService;
 
-    //Call this method when the request is initiated from login-jsp and the method is GET.
-    //This method will return the login page at the first instantiation.
-    @RequestMapping(value="/login-jsp", method= RequestMethod.GET)
-    public String goToLoginPage()
+    //Call this method when the request is initiated when a user access the root of the application.
+    //This method will return the welcome page at the first instantiation.
+    @RequestMapping(value="/", method= RequestMethod.GET)
+    public String goToWelcomePage(ModelMap modelMap)
     {
-        logger.debug ("Into Login Page");
-        return "Login";
+        logger.debug ("Into goToWelcomePage method");
+        modelMap.put("name",getLoggedInUserName());
+        return "Welcome";
     }
+
+    private String getLoggedInUserName()
+    {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String loggedInUserName = authentication.getName();
+        return loggedInUserName;
+
+    }
+
+ /*
 
     //Call this method when the request is initiated after entering the username and password
     // from login-jsp and the method is POST
@@ -39,6 +51,8 @@ public class LoginUIController
     {
         logger.debug("Into goToWelcome controller method");
         boolean authenticated = authenticationService.authenticateUser(userName,password);
+
+        //If the login is successful, the page navigates to Welcome.jsp
         if (authenticated) {
             logger.debug("Authentication successful");
             modelMap.put("name", userName);
@@ -48,5 +62,5 @@ public class LoginUIController
             logger.debug("Authentication not successful");
             return "Login";
         }
-    }
+   */
 }
