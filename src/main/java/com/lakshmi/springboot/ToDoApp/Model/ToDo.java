caa589c33744ddf44484bb179e74efb0ccfaca01
lakshmibaskaran <40994781+lakshmibaskaran@users.noreply.github.com>
2023-01-10 06:31:25 -0500

    Created annotations for NotNull and Size in Bean.

--- src/main/java/com/lakshmi/springboot/ToDoApp/Model/ToDo.java
+++ src/main/java/com/lakshmi/springboot/ToDoApp/Model/ToDo.java
@@ -1,18 +1,29 @@
 package com.lakshmi.springboot.ToDoApp.Model;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 
 public class ToDo
 {
+@NotNull
 private int toDoId;
 private String userName;
+@Size(min=10, message = "Description should be at least 10 characters")
 private String toDoDescription;
+//@FutureOrPresent
 private LocalDate toDoByDate;
 private boolean toDoActivityFinished;
 
+private Logger logger = LoggerFactory.getLogger((getClass()));
 
-    public ToDo(int toDoId, String userName, String toDoDescription, LocalDate toDoByDate, boolean toDoActivityFinished) {
+    public ToDo(int toDoId, String userName, String toDoDescription, LocalDate toDoByDate, boolean toDoActivityFinished)
+    {
         this.toDoId = toDoId;
         this.userName = userName;
         this.toDoDescription = toDoDescription;
@@ -21,43 +32,53 @@ private boolean toDoActivityFinished;
     }
 
 
-    public int getToDoId() {
+    public int getToDoId()
+    {
         return toDoId;
     }
 
-    public String getUserName() {
+    public String getUserName()
+    {
         return userName;
     }
 
-    public String getToDoDescription() {
+    public String getToDoDescription()
+    {
         return toDoDescription;
     }
 
-    public LocalDate getToDoByDate() {
+    public LocalDate getToDoByDate()
+    {
         return toDoByDate;
     }
 
-    public boolean isToDoActivityFinished() {
+    public boolean isToDoActivityFinished()
+    {
         return toDoActivityFinished;
     }
 
-    public void setToDoId(int toDoId) {
+    public void setToDoId(int toDoId)
+    {
         this.toDoId = toDoId;
     }
 
-    public void setUserName(String userName) {
+    public void setUserName(String userName)
+    {
         this.userName = userName;
     }
 
-    public void setToDoDescription(String toDoDescription) {
+    public void setToDoDescription(String toDoDescription)
+    {
         this.toDoDescription = toDoDescription;
     }
 
-    public void setToDoByDate(LocalDate toDoByDate) {
+    public void setToDoByDate(LocalDate toDoByDate)
+    {
         this.toDoByDate = toDoByDate;
     }
 
-    public void setToDoActivityFinished(boolean toDoActivityFinished) {
+    public void setToDoActivityFinished(boolean toDoActivityFinished)
+    {
         this.toDoActivityFinished = toDoActivityFinished;
     }
 
